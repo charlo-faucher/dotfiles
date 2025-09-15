8994c45452de312d7d617eb132a062fc0214e17a
charlo-faucher <fauchercharlo@gmail.com>
2025-09-15 09:16:20 -0400

    add fonts

--- setup/base/arch.sh
+++ setup/base/arch.sh
@@ -5,3 +5,8 @@ echo ">>> Installing base packages for Arch..."
 
 pacman -Syu --noconfirm
 pacman -S --noconfirm sudo git stow zsh tmux neovim tree fzf ripgrep curl wget
+
+# Install NerdFonts
+yay -S nerd-fonts-fira-code
+pacman -S --noconfirm ttf-font-awesome powerline-fonts
+
