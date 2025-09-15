2e7b5b40a272cf4a24a64db247cd3f9da5412e49
charlo-faucher <fauchercharlo@gmail.com>
2025-09-15 00:54:40 -0400

    edit copy for tmux

--- install.sh
+++ install.sh
@@ -53,8 +53,13 @@ stow -d "$DOTFILES_DIR/config" -t $HOME zsh nvim tmux
 
 echo ">>> Ensuring tmux configuration..."
 if [ ! -f "$HOME/.config/tmux/tmux.conf" ]; then
-  cp "$HOME/.config/tmux/.tmux.conf.local" \
+  ln -s "$HOME/.config/tmux/.tmux.conf" \
      "$HOME/.config/tmux/tmux.conf"
+fi
+
+if [ ! -f "$HOME/.config/tmux/tmux.conf.local" ]; then
+  cp "$HOME/.config/tmux/.tmux.conf.local" \
+     "$HOME/.config/tmux/tmux.conf.local"
 fi
 
 echo ">>> Done! Restart your shell to apply changes."
