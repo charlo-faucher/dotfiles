6c36017071e61cdb30ddb998dbc22089d874aeae
charlo-faucher <fauchercharlo@gmail.com>
2025-09-15 09:22:27 -0400

    add yay

--- setup/base/arch.sh
+++ setup/base/arch.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 echo ">>> Installing base packages for Arch..."
 
 pacman -Syu --noconfirm
-pacman -S --noconfirm sudo git stow zsh tmux neovim tree fzf ripgrep curl wget yay
+pacman -S --noconfirm sudo git stow zsh tmux neovim tree fzf ripgrep curl wget
+pacman -S --noconfirm --needed base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 # Install NerdFonts
 yay -S nerd-fonts-fira-code
