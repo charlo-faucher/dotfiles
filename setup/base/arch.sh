2ffb9edc4c3a104966d215034926ca0100477341
charlo-faucher <fauchercharlo@gmail.com>
2025-09-15 01:28:48 -0400

    oopsi, forgor &

--- setup/base/arch.sh
+++ setup/base/arch.sh
@@ -6,4 +6,4 @@ echo ">>> Installing base packages for Arch..."
 {
     pacman -Syu --noconfirm
     pacman -S --noconfirm sudo git stow zsh tmux neovim tree fzf ripgrep curl wget
-} > /dev/null
+} &> /dev/null
