db1ab795ef5df707d7abef73b50e062d7a01341e
charlo-faucher <fauchercharlo@gmail.com>
2025-09-15 09:31:29 -0400

    comment non working parts

--- setup/base/arch.sh
+++ setup/base/arch.sh
@@ -5,9 +5,9 @@ echo ">>> Installing base packages for Arch..."
 
 pacman -Syu --noconfirm
 pacman -S --noconfirm sudo git stow zsh tmux neovim tree fzf ripgrep curl wget
-pacman -S --noconfirm --needed base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+#pacman -S --noconfirm --needed base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 # Install NerdFonts
-yay -S nerd-fonts-fira-code
-pacman -S --noconfirm ttf-font-awesome powerline-fonts
+#yay -S nerd-fonts-fira-code
+#pacman -S --noconfirm ttf-font-awesome powerline-fonts
 
