790808acb2cf72b1b5d22138f2920be0f68a88d9
charlo-faucher <fauchercharlo@gmail.com>
2025-09-16 10:05:18 -0400

    fix link.sh

--- link.sh
+++ link.sh
@@ -1,22 +1,21 @@
+#!/usr/bin/env bash
+
 DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 cd "$DOTFILES_DIR"
-echo "AAAAAAAAAAAA"
 
 stow -d "$DOTFILES_DIR/config" -t $HOME zsh nvim
-echo "BBBBBBBBBBBB"
 
 mkdir -p "$HOME/.config/tmux"
-
-echo "CCCCCCCCCCCCCC"
 
 if [ ! -f "$HOME/.config/tmux/tmux.conf" ]; then
   ln -s "$DOTFILES_DIR/config/tmux/.tmux/.tmux.conf" \
      "$HOME/.config/tmux/tmux.conf" 
 fi
 
-echo "DDDDDDDDDDD"
-
 if [ ! -f "$HOME/.config/tmux/tmux.conf.local" ]; then
   cp "$DOTFILES_DIR/config/tmux/.tmux.conf.local" \
      "$HOME/.config/tmux/tmux.conf.local"
 fi
+
+cd "$HOME"
