2c812f1872de622c145230b31e8de1e091d8ce65
charlo-faucher <fauchercharlo@gmail.com>
2025-09-16 09:48:32 -0400

    add verbose

--- link.sh
+++ link.sh
@@ -1,14 +1,20 @@
 DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$DOTFILES_DIR"
+echo "AAAAAAAAAAAA"
 
 stow -d "$DOTFILES_DIR/config" -t $HOME zsh nvim
+echo "BBBBBBBBBBBB"
 
 mkdir -p "$HOME/.config/tmux"
+
+echo "CCCCCCCCCCCCCC"
 
 if [ ! -f "$HOME/.config/tmux/tmux.conf" ]; then
   ln -s "$DOTFILES_DIR/config/tmux/.tmux/.tmux.conf" \
      "$HOME/.config/tmux/tmux.conf" 
 fi
+
+echo "DDDDDDDDDDD"
 
 if [ ! -f "$HOME/.config/tmux/tmux.conf.local" ]; then
   cp "$DOTFILES_DIR/config/tmux/.tmux.conf.local" \
